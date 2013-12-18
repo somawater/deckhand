98fbd6935f4b43a5debd6a5340d7f31972a00c23
Lawrence Wang <l@lw.io>
2013-12-17 16:02:17 -0800

    integrate with the asset pipeline

--- lib/deckhand/engine.rb
+++ lib/deckhand/engine.rb
@@ -8,8 +8,17 @@ module Deckhand
       Deckhand::Configuration.instance.load_initializer_block
     end
 
-    initializer :setup_browserify, :after => "sprockets.environment", :group => :all do |app|
+    initializer 'deckhand.setup_browserify', :after => "sprockets.environment", :group => :all do |app|
       app.assets.register_postprocessor 'application/javascript', Sprockets::Browserify
+    end
+
+    initializer 'deckhand.assets_precompile', :group => :all do |app|
+      app.config.assets.precompile += %w[
+        deckhand/bootstrap-3.0.3/css/bootstrap.css
+        deckhand/index.css
+        deckhand/theme.css
+        deckhand/index.js
+      ]
     end
 
   end
