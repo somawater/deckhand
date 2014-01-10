87f51afb1d871b4de789962dc3309e4d781f109e
Lawrence Wang <l@lw.io>
2014-01-10 12:12:42 -0800

    improve angular_binding helper method
    so it handles links and html values correctly

--- app/helpers/deckhand/templates_helper.rb
+++ app/helpers/deckhand/templates_helper.rb
@@ -4,19 +4,21 @@ module Deckhand::TemplatesHelper
     Deckhand.config.fields_to_show @model
   end
 
-  def angular_binding(field, parent = 'item')
+  def angular_binding(field, options = {})
+    parent = options[:parent] || 'item'
+
     if Deckhand.config.link?(@model, field)
-      trusted_html do
-        ng_click = "open(#{Deckhand.config.model_name_for(@model, field)}, '{{item.id}}')"
-        content_tag :a, "{{#{parent}.#{field}._label}}", 'ng-click' => ng_click
-      end
+      ng_click = "open('#{Deckhand.config.model_name_for(@model, field)}', item.#{field}.id)"
+      content_tag :a, "{{#{parent}.#{field}._label}}", 'ng-click' => ng_click
+    elsif options[:html]
+      trusted_html "{{#{parent}.#{field}}}"
     else
       "{{#{parent}.#{field} | prettify}}"
     end
   end
 
-  def trusted_html(&block)
-    content_tag :div, '', 'ng-bind-html' => "raw('#{block.call}')"
+  def trusted_html(string = nil, &block)
+    content_tag :div, '', 'ng-bind-html' => "raw('#{string || block.call}')"
   end
 
   def relations_to_tabulate
