cb0a88842eef3eb810db633b75b67add8d3b193b
Lawrence Wang <l@lw.io>
2014-01-11 13:32:55 -0800

    support CSRF protection

--- app/controllers/deckhand/base_controller.rb
+++ app/controllers/deckhand/base_controller.rb
@@ -1,2 +1,15 @@
 class Deckhand::BaseController < ApplicationController
+
+  after_filter :set_csrf_cookie
+
+  protected
+
+  def set_csrf_cookie
+    cookies['XSRF-TOKEN'] = form_authenticity_token
+  end
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+
 end
