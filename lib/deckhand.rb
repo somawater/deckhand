f4db819ba25f33fcd32b69bea373e3a0fa6c3e2d
Lawrence Wang <l@lw.io>
2014-01-21 17:05:47 -0800

    add #try

--- lib/deckhand.rb
+++ lib/deckhand.rb
@@ -1,5 +1,6 @@
 require 'active_support/inflector'
 require 'active_support/core_ext/module/delegation'
+require 'active_support/core_ext/object/try'
 require 'deckhand/configuration'
 require 'deckhand/search'
 require 'deckhand/presenter'
