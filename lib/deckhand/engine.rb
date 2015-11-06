886d76a2b8fd061777ab9d80dc9b2d5a181590ee
Pavan Trikutam <pavan@graybike.co>
2015-11-06 11:41:43 -0800

    remove non-extension filenames from asset precompilation

--- lib/deckhand/engine.rb
+++ lib/deckhand/engine.rb
@@ -16,6 +16,18 @@ module Deckhand
     end
 
     initializer 'deckhand.assets_precompile', :group => :all do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+          '.html', '.erb', '.haml',                 # Templates
+          '.png',  '.gif', '.jpg', '.jpeg',         # Images
+          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+      end)
+      
       app.config.assets.precompile += %w[
         deckhand/bootstrap-3.0.3/css/bootstrap.css
         deckhand/index.css
